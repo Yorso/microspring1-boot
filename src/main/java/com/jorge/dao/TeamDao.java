6f1a9265598e000ab0c43ab2d12c5bbd1f665d3d
Yorso <jorgeguzon@gmail.com>
2017-01-03 16:25:01 +0100

    Spring Data JPA

---
+++ src/main/java/com/jorge/dao/TeamDao.java
@@ -0,0 +1,15 @@
+package com.jorge.dao;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.jorge.domain.Team;
+
+
+public interface TeamDao extends CrudRepository<Team, Long> { //CrudRepository es un repositorio proporcionado por la dependencia spring-boot-starter-data-jpa (Spring Data JPA library)
+
+	List<Team> findAll(); //Spring Data is going to know how to implement that method for me
+	
+	Team findByName(String name); //Spring Data is going to know how to implement that method for me
+}
